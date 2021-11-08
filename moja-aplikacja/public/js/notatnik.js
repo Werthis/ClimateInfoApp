f7f12249d888457ad206c3356d6d2e7db99ac49f
Janko Walski <lukaszstawecki@gmail.com>
2021-11-08 15:25:18 +0100

    vip

--- moja-aplikacja/public/js/notatnik.js
+++ moja-aplikacja/public/js/notatnik.js
@@ -32,7 +32,12 @@ const rows = [
     cccma_cgcm3_1: 23.43,
     cnrm_cm3: 35.56,
   },
-  { month: 'March', bccr_bcm2_0: 12.234, cccma_cgcm3_1: 23.43, cnrm_cm3: 35.56 },
+  {
+    month: "March",
+    bccr_bcm2_0: 12.234,
+    cccma_cgcm3_1: 23.43,
+    cnrm_cm3: 35.56,
+  },
   { month: 4, bccr_bcm2_0: "Stark", cccma_cgcm3_1: "Arya", cnrm_cm3: 16 },
   {
     month: 5,
@@ -45,3 +50,26 @@ const rows = [
   { month: 8, bccr_bcm2_0: "Frances", cccma_cgcm3_1: "Rossini", cnrm_cm3: 36 },
   { month: 9, bccr_bcm2_0: "Roxie", cccma_cgcm3_1: "Harvey", cnrm_cm3: 65 },
 ];
+
+
+
+for (let i = 0; i < listOfMonths.length; i++) {
+  row.push({
+    month: listOfMonths[i],
+    bccr_bcm2_0: list_bccr_bcm2_0[i],
+    cccma_cgcm3_1: list_cccma_cgcm3_1[i],
+    cnrm_cm3: list_cnrm_cm3[i],
+    csiro_mk3_5: list_csiro_mk3_5[i],
+    gfdl_cm2_0: list_gfdl_cm2_0[i],
+    gfdl_cm2_1: list_gfdl_cm2_1[i],
+    ingv_echam4: list_ingv_echam4[i],
+    inmcm3_0: list_inmcm3_0[i],
+    ipsl_cm4: list_ipsl_cm4[i],
+    miroc3_2_medres: list_miroc3_2_medres[i],
+    miub_echo_g: list_miub_echo_g[i],
+    mpi_echam5: list_mpi_echam5[i],
+    mri_cgcm2_3_2a: list_mri_cgcm2_3_2a[i],
+    ukmo_hadcm3: list_ukmo_hadcm3[i],
+    ukmo_hadgem1: list_ukmo_hadgem1[i],
+  });
+}
--- moja-aplikacja/src/App.js
+++ moja-aplikacja/src/App.js
@@ -38,11 +38,28 @@ const ModelResults = (props) => {
     "November",
     "December",
   ];
-  const listOfModels = [];
-  const [rowDict, setRowDict] = useState({});
-  const columns = [{ field: "month", headerName: "Month", width: 150 }];
 
-  const firstRow = ["Model type"];
+  const list_bccr_bcm2_0 = [];
+  const list_cccma_cgcm3_1 = [];
+  const list_cnrm_cm3 = [];
+  const list_csiro_mk3_5 = [];
+  const list_gfdl_cm2_0 = [];
+  const list_gfdl_cm2_1 = [];
+  const list_ingv_echam4 = [];
+  const list_inmcm3_0 = [];
+  const list_ipsl_cm4 = [];
+  const list_miroc3_2_medres = [];
+  const list_miub_echo_g = [];
+  const list_mpi_echam5 = [];
+  const list_mri_cgcm2_3_2a = [];
+  const list_ukmo_hadcm3 = [];
+  const list_ukmo_hadgem1 = [];
+
+  const listOfModels = [];
+  const listaPomocnicza = [];
+  const columns = [{ field: "month", headerName: "Month", width: 150 }];
+  const row = [];
+
   const rows = [];
 
   props.items.map((Object) => {
@@ -51,7 +68,8 @@ const ModelResults = (props) => {
     return rows;
   });
   console.log("ROWS!!", rows);
-  for (var i = 0; i < rows.length; i++) {
+  console.log("1st ROW!!", rows[0]);
+  for (let i = 0; i < rows.length; i++) {
     columns.push({
       field: listOfModels[i],
       headerName: listOfModels[i],
@@ -59,13 +77,59 @@ const ModelResults = (props) => {
       editable: true,
     });
   }
+  // console.log("listOfModels      ", listOfModels);
+  for (let i = 0; i < rows.length; i++) {
+    listaPomocnicza.push(listOfModels[i], rows[i]);
+    // listaPomocnicza.push(rows[i] );
+  }
 
-  rows.map((item) => {
-    for (var i = 0; i < item.length; i++) {
-      console.log(item[i], listOfMonths[i]);
-    }
-    console.log("-----------------------------------");
-  });
+  console.log("listaPomocnicza         ", listaPomocnicza);
+  list_bccr_bcm2_0.push(rows[0]);
+  console.log("lista bcc", list_bccr_bcm2_0);
+  list_cccma_cgcm3_1.push(rows[1]);
+  console.log("lista ccc", list_cccma_cgcm3_1);
+
+  list_cnrm_cm3.push(rows[2]);
+  list_csiro_mk3_5.push(rows[3]);
+  list_gfdl_cm2_0.push(rows[4]);
+  list_gfdl_cm2_1.push(rows[5]);
+  list_ingv_echam4.push(rows[6]);
+  list_inmcm3_0.push(rows[7]);
+  list_ipsl_cm4.push(rows[8]);
+  list_miroc3_2_medres.push(rows[9]);
+  list_miub_echo_g.push(rows[10]);
+  list_mpi_echam5.push(rows[11]);
+  list_mri_cgcm2_3_2a.push(rows[12]);
+  list_ukmo_hadcm3.push(rows[13]);
+  list_ukmo_hadgem1.push(rows[14]);
+  // rows.map((item) => {
+  //   for (var i = 0; i < item.length; i++) {
+  //     console.log(item[i], listOfMonths[i]);
+  //   }
+  //   console.log("-----------------------------------");
+  // });
+  for (let i = 0; i < listOfMonths.length; i++) {
+    row.push({
+      month: listOfMonths[i],
+      bccr_bcm2_0: list_bccr_bcm2_0[i],
+      cccma_cgcm3_1: list_cccma_cgcm3_1[i],
+      cnrm_cm3: list_cnrm_cm3[i],
+      csiro_mk3_5: list_csiro_mk3_5[i],
+      gfdl_cm2_0: list_gfdl_cm2_0[i],
+      gfdl_cm2_1: list_gfdl_cm2_1[i],
+      ingv_echam4: list_ingv_echam4[i],
+      inmcm3_0: list_inmcm3_0[i],
+      ipsl_cm4: list_ipsl_cm4[i],
+      miroc3_2_medres: list_miroc3_2_medres[i],
+      miub_echo_g: list_miub_echo_g[i],
+      mpi_echam5: list_mpi_echam5[i],
+      mri_cgcm2_3_2a: list_mri_cgcm2_3_2a[i],
+      ukmo_hadcm3: list_ukmo_hadcm3[i],
+      ukmo_hadgem1: list_ukmo_hadgem1[i],
+    });
+  }
+  console.log(row);
+
   console.log(columns);
   for (var i = 0; i < rows.length; i++) {
     for (var j = 0; j < listOfMonths.length; j++) {}
@@ -151,7 +215,7 @@ const App = () => {
       : isoCode;
   };
 
-  const allMonthsValues = [];
+  // const allMonthsValues = [];
 
   const useStyles = makeStyles((theme) => ({
     root: {
